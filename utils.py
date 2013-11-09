74be6a8988cd29a2f27a22c0f0384c3617a0ef9b
Jonathan Goldsmith <jhghank@gmail.com>
2013-11-08 22:01:35 -0500

    coroutine

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+def coroutine(func):
+    def start(*args,**kwargs):
+        cr = func(*args,**kwargs)
+        cr.next()
+        return cr 
+    return start
+
+@coroutine
 def pipe():
 	"""
 	Create a pipe that takes input asynchronously then yields it.
