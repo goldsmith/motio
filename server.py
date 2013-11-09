7c84e6428dc62cfc97f23335db84cad310d70e2f
Jonathan Goldsmith <jhghank@gmail.com>
2013-11-09 08:57:12 -0500

    redis saves the day

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import os
+import redis
+import gevent
 
 from flask import Flask, request
 from flask_sockets import Sockets
@@ -9,8 +11,50 @@ from utils import pipe
 app = Flask(__name__)
 sockets = Sockets(app)
 
-web_socket = None
-# web_socket_pipe = ("foo", "bar", "foobar", "barfoo", "bar food", "nighttime")
+REDIS_URL = os.environ.get('REDISCLOUD_URL')
+REDIS_CHAN = 'motio'
+
+if not REDIS_URL:
+	redis = redis.Redis()
+else:
+	redis = redis.from_url(REDIS_URL)
+
+socket = None
+# socket_pipe = ("foo", "bar", "foobar", "barfoo", "bar food", "nighttime")
+
+class SocketBackend(object):
+
+	def __init__(self):
+		self.clients = []
+		self.pubsub = redis.pubsub()
+		self.pubsub.subscribe(REDIS_CHAN)
+
+	def __iter_data(self):
+		for message in self.pubsub.listen():
+			data = message.get('data')
+			print 'data received: {}'.format(data)
+			if message['type'] == 'message':
+				yield data
+
+	def register(self, client):
+		self.clients.append(client)
+
+	def send(self, client, data):
+		try:
+			client.send(data)
+		except:
+			self.clients.remove(client)
+
+	def run(self):
+		for data in self.__iter_data():
+			for client in self.clients:
+				gevent.spawn(self.send, client, data)
+
+	def start(self):
+		gevent.spawn(self.run)
+
+socket = SocketBackend()
+socket.start()
 
 @app.route("/")
 def index():
@@ -27,12 +71,19 @@ def add_gesture():
 
 	train(data, name)
 
-	web_socket_pipe.send({
+	socket_pipe.send({
 		"name": name,
 		"action": "add_gesture"
 	})
 
 	return
+
+@app.route("/damn")
+def damn():
+	redis.publish(REDIS_CHAN, "foo")
+	print socket
+
+	return "bye!"
 
 @app.route("/do_gesture")
 def do_gesture():
@@ -40,19 +91,19 @@ def do_gesture():
 	name = predict(data)
 	# test the model
 	# let the client know of the command name
-	web_socket.send({
+	redis.publish(REDIS_CHAN, {
 		"name": name,
 		"action": "do_gesture"
 	})
 
 @sockets.route("/client_socket")
 def web_socket(ws):
-	print "connected", ws
+	socket.register(ws)
 
-	web_socket = ws
+	while ws.socket is not None:
+		gevent.sleep()
 
-	while True:
-		continue
+
 
 # if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 5000))
