1f1309558c7132891a8d7380fc7f6ba256cda42e
Jonathan Goldsmith <jhghank@gmail.com>
2013-11-09 12:36:35 -0500

    redis

--- match.py
+++ match.py
@@ -1,8 +1,12 @@
 import numpy as np
+import pickle
 
 from sklearn.neighbors import KNeighborsClassifier
+from server import r
 
 clf = KNeighborsClassifier(1)
+if not r.get("clf"):
+	r.set("clf", pickle.dumps(clf))
 
 def train(data, label):
 	"""
@@ -10,13 +14,20 @@ def train(data, label):
 	label should be a string
 	"""
 
+	clf = pickle.loads(r.get("clf"))
+
 	flat = np.array(data).flatten()
 	clf.fit([flat], [label])
+
+	r.set("clf", pickle.dumps(clf))
 
 def predict(data):
 	"""
 	Same form
 	"""
 
+	clf = pickle.loads(r.get("clf"))
+
 	flat = np.array(data).flatten()
 	return clf.predict(flat)
+
